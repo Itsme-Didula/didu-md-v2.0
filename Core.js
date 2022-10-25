9224c92954f50a4d95d1fc013f12bb9b54b708df
Kai... !! <76661641+Kai0071@users.noreply.github.com>
2022-10-25 12:47:45 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -4708,21 +4708,7 @@ const helpmenu = `Hemlo *${pushname}* Dear...!! ,
 │╰────────────────···▸
 │╭────────────────···▸
 ┴│▸
-⬡│▸ // program to display the date
-// get local machine date time
-const date = new Date();
-
-// get the date as a string
-const n = date.toDateString();
-
-// get the time as a string
-const time = date.toLocaleTimeString();
-
-// display date
-console.log('Date: ' + n);
-
-// display time
-console.log('Time: ' + time);
+⬡│▸ *Time* :
 ┬│▸
 │╰────────────────···▸
 ┠───═[ *BOT INFO* ]═──▸

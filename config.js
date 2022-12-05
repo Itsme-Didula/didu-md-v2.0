47dba738d8a035eeb9f32976712152bf4c9692d1
Kai... !! <76661641+Kai0071@users.noreply.github.com>
2022-12-05 12:13:36 +0000

    Auto typing added

--- config.js
+++ config.js
@@ -6,6 +6,9 @@ global.autoReadGc = true
 global.autoReadAll = false
 global.antitags = true
 
+//aumto functioner
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 
 global.Owner = ['916297175943']
 global.OwnerNumber = ['916297175943']

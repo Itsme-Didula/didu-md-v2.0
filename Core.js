22a45e27c19ef8ce85303c557f1c3c206f65dff3
Kai... !! <76661641+Kai0071@users.noreply.github.com>
2023-01-02 12:13:41 +0000

    Bohat thand lag raha...

--- Core.js
+++ Core.js
@@ -2582,7 +2582,23 @@ case 'listonline': case 'here':{
  }
  break
 		
-		
+		case 'speedtest': case 'speedcheck': {
+             m.reply(`Plz Wait ${pushname} Testing Speed... ⚙️`)
+             let cp = require('child_process')
+             let { promisify } = require('util')
+             let exec = promisify(cp.exec).bind(cp)
+           let o
+           try {
+           o = await exec('python speed.py')
+           } catch (e) {
+           o = e
+          } finally {
+         let { stdout, stderr } = o
+         if (stdout.trim()) m.reply(stdout)
+         if (stderr.trim()) m.reply(stderr)
+             }
+             }
+             break
 		
 		
 		

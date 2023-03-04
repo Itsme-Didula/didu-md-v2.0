4877aaf4e736734bac5ef5ef5405a0894379a001
Kai... !! <76661641+Kai0071@users.noreply.github.com>
2023-03-04 10:37:47 +0000

    Update Core.js

--- Core.js
+++ Core.js
@@ -4100,9 +4100,12 @@ reply(mess.error)
   A17.sendMessage(from, { audio: { url: A17tiktokaudio }, mimetype: 'audio/mp4' }, { quoted: m })
  }
 break
+		
+		
 case 'music': case 'p': case 'play': case 'song': case 'ytplay': {
     if (isBan) return reply(mess.banned)	 			
- if (isBanChat) return reply(mess.bangc)
+ if (isBanChat) return reply(mess.bangc
+ A17.sendMessage(from, { react: { text: "📃" , key: m.key }}) 
  const YT=require('./lib/ytdlcore')
  const { isUrl, fetchBuffer } = require('./lib/Function')
 
